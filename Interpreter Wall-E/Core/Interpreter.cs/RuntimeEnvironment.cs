53ddb057642ff33349340ca459f6810d278a5821
Diana <dianama224422@gmail.com>
2025-06-11 19:34:25 -0400

    Symbol Table added

---
+++ Interpreter Wall-E/Core/Interpreter.cs/RuntimeEnvironment.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using Interpreter.Core.Ast.Statements; // For ProgramNode
+
+namespace Interpreter.Core.Interpreter
+{
+    public class RuntimeEnvironment
+    {
+
+    }
+}
